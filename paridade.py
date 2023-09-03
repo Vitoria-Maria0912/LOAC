99cdbf04a62fbbaa6c29d43ed7b543600ee70409
Vitória Maria do Nascimento <116987726+Vitoria-Maria0912@users.noreply.github.com>
2023-09-02 21:55:45 -0300

    Update paridade.py

--- paridade.py
+++ paridade.py
@@ -1,26 +1,59 @@
-def calcular_paridade(bits):
-    count_ones = sum(bit for bit in bits)  # Conta o número de bits '1'
-    paridade = count_ones % 2  # Calcula a paridade (0 se par, 1 se ímpar)
+def checa_paridade_par(bits):
+    
+    quantidade_uns = bits.count("1")
+    bit_paridade = ""
+            
+    if quantidade_uns % 2 == 0:
+        bit_paridade = '0'
+    else: 
+        bit_paridade = '1'
+        
+    return bit_paridade
+
+
+def verifica_paridade(bits):
+    
+    bit_paridade = bits[0]
+    dados = bits[1:]
+    paridade = True
+    
+    paridade_calculada = checa_paridade_par(dados)
+    
+    if(paridade_calculada != bit_paridade):
+        paridade = False
+        
     return paridade
+    
 
+bits_originais = input("Digite bit a bit, sem separá-los por espaços: ")
+bit_paridade = checa_paridade_par(bits_originais)
+dados_transmitidos = bit_paridade + bits_originais
 
-def adicionar_paridade(bits):
-    paridade = calcular_paridade(bits)
-    bits_com_paridade = bits + [paridade]
-    return bits_com_paridade
+print(f"Dados transmitidos: {dados_transmitidos}\n")
 
+bits_corrompidos = input("Digite bit a bit, os dados supostamente corrompidos: ")
+verificacao = verifica_paridade(bits_corrompidos)
 
-def verificar_paridade(bits_com_paridade):
-    paridade_calculada = calcular_paridade(bits_com_paridade[:-1])  # Calcula paridade sem o último bit
-    paridade_recebida = bits_com_paridade[-1]  # Último bit é a paridade recebida
-    if paridade_calculada == paridade_recebida:
-        return "Sem erros de paridade."
-    else:
-        return "Erro de paridade detectado."
+if(verificacao):
+    print(f"Não foi detectado erro de paridade.")
+else:
+    print(f"Erro de paridade detectado, os dados são incompatíveis.")
 
-# Exemplo de uso
-bits_originais = [1, 0, 1, 1, 0]  # Bits de dados originais
-bits_com_paridade = adicionar_paridade(bits_originais)  # Adiciona bit de paridade
-print("Bits originais:", bits_originais)
-print("Bits com paridade:", bits_com_paridade)
-print(verificar_paridade(bits_com_paridade))
+    
+# Exemplo1:
+#
+# bits_originais = "101010"  
+# bit_paridade = "1"
+# Dados transmitidos: 1101010
+# bits_corrompidos = "101010"
+#
+# Saída: "Não foi detectado erro de paridade."
+
+# Exemplo2:
+#
+# bits_originais = "110011"  
+# bit_paridade = "0"
+# Dados transmitidos: 0110011
+# bits_corrompidos = "0110010"
+#
+# Saída: "Erro de paridade detectado, os dados são incompatíveis."
